7f0049396780906c6d2a464c684a79b96a1b5cf6
Aromee Kim <aromeekim@aromees-mbp.home>
2021-04-26 14:45:45 -0700

    MERN-Stack Infrastructure

--- server.js
+++ server.js
@@ -16,13 +16,18 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Middleware to verify token and assign user object of payload to req.user
+// Be sure to mount before our routes
+app.use(require('./config/checkToken'));
+
 // Configure to use port 3001 instead of 3000 during development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
 app.listen(port, function () {
 	console.log(`Express app running on port ${port}`);
 });
 
-//api routes will go here
+//API routes will go here
+app.use('/api/users', require('./routes/api/users'));
 
 // The following "catch all" route (note the *) is necessary
 app.get('/*', function (req, res) {
